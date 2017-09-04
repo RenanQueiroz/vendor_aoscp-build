2fe5cb3f4a2926e2cf1f75aba4194c04b896448c
Simon Shields <simon@lineageos.org>
2017-09-04 21:11:16 +1000

    build: soong: put aoscp variables in the "Aoscp" struct
    Change-Id: I9ed55cea0c55f0078f8f97ad4037b16ed11c0156

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,5 +1,8 @@
+# Insert new variables inside the CypherOS structure
 aoscp_soong:
 	$(hide) mkdir -p $(dir $@)
 	$(hide) (\
 	echo '{'; \
+	echo '"Aoscp": {'; \
+	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
